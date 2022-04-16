fb709018cd7fdca59599f423135164ae642f18f7
wwqdrh <>
2022-04-16 13:58:05 +0000

    Update from https://github.com/wwqdrh/gitpage/commit/8408a1db2ff4b55f5d325d71d77a8eca71a9a4d6

--- algorithm/leetcode/leet2104/2104.子数组范围和.go
+++ algorithm/leetcode/leet2104/2104.子数组范围和.go
@@ -1,5 +1,10 @@
 package leet2104
 
+import (
+	"container/heap"
+	"sort"
+)
+
 /*
  * @lc app=leetcode.cn id=2104 lang=golang
  *
@@ -7,69 +12,54 @@ package leet2104
  */
 
 // @lc code=start
-func subArrayRanges(nums []int) int {
+
+func subArrayRanges2(nums []int) (ans int64) {
+	for i, num := range nums {
+		minVal, maxVal := num, num
+		for _, v := range nums[i+1:] {
+			if v < minVal {
+				minVal = v
+			} else if v > maxVal {
+				maxVal = v
+			}
+			ans += int64(maxVal - minVal)
+		}
+	}
+	return
+}
+
+func subArrayRanges(nums []int) int64 {
+	var res int64
 	n := len(nums)
-	min, max := getCnt(nums, true), getCnt(nums, false)
-	res := 0
+
 	for i := 0; i < n; i++ {
-		res += (max[i] - min[i]) * nums[i]
+		min, max := new(minHeap), new(maxHeap)
+		for j := i; j < n; j++ {
+			heap.Push(min, nums[j])
+			heap.Push(max, -nums[j])
+			res += int64(-max.IntSlice[0] - min.IntSlice[0])
+		}
 	}
 	return res
 }
 
-// 获取左边最近最小的个数 右边最近最小的个数
-// 使用单调栈记录最小元素
-func getCnt(num []int, isMin bool) []int {
-	n := len(num)
-	left, right := make([]int, n), make([]int, n)
+type minHeap struct {
+	sort.IntSlice
+}
+type maxHeap struct {
+	sort.IntSlice
+}
 
-	stack := []int{} // 单调栈
-	// 计算左边
-	for i := 0; i < n; i++ {
-		for len(stack) > 0 {
-			if isMin && num[stack[len(stack)-1]] >= num[i] {
-				// 记录最小值的索引，将大于它的弹出去
-				stack = stack[:len(stack)-1]
-			} else if !isMin && num[stack[len(stack)-1]] <= num[i] {
-				stack = stack[:len(stack)-1]
-			} else {
-				break
-			}
-		}
-		idx := -1
-		if len(stack) != 0 {
-			idx = stack[len(stack)-1]
-		}
-		left[i] = idx
-		stack = append(stack, i)
-	}
-
-	// 计算右边
-	stack = stack[:0]
-	for i := n - 1; i >= 0; i-- {
-		for len(stack) > 0 {
-			if isMin && num[stack[len(stack)-1]] >= num[i] {
-				// 记录最小值的索引，将大于它的弹出去
-				stack = stack[:len(stack)-1]
-			} else if !isMin && num[stack[len(stack)-1]] <= num[i] {
-				stack = stack[:len(stack)-1]
-			} else {
-				break
-			}
-		}
-		idx := -1
-		if len(stack) != 0 {
-			idx = stack[len(stack)-1]
-		}
-		right[i] = idx
-		stack = append(stack, i)
-	}
-
-	// 计算
-	res := make([]int, n)
-	for i := 0; i < n; i++ {
-		res[i] = (i - left[i]) * (right[i] - i)
-	}
+func (h *minHeap) Push(val interface{}) { h.IntSlice = append(h.IntSlice, val.(int)) }
+func (h *minHeap) Pop() interface{} {
+	res := h.IntSlice[len(h.IntSlice)-1]
+	h.IntSlice = h.IntSlice[:len(h.IntSlice)-1]
+	return res
+}
+func (h *maxHeap) Push(val interface{}) { h.IntSlice = append(h.IntSlice, val.(int)) }
+func (h *maxHeap) Pop() interface{} {
+	res := h.IntSlice[len(h.IntSlice)-1]
+	h.IntSlice = h.IntSlice[:len(h.IntSlice)-1]
 	return res
 }
 
--- algorithm/leetcode/leet6/6.z-字形变换.go
+++ algorithm/leetcode/leet6/6.z-字形变换.go
@@ -1,7 +1,5 @@
 package leet6
 
-import "strings"
-
 /*
  * @lc app=leetcode.cn id=6 lang=golang
  *
@@ -10,47 +8,22 @@ import "strings"
 
 // @lc code=start
 func convert(s string, numRows int) string {
-	// 不需要模拟 找规律每行各个id的规律
-	// 当一行的时候: x+1
-	// 当两行的时候: 1: x+2, 2: x+2
-	// 当三行的时候: 1: x+4, 2: x+2, 3: x+4
-	// 当四行的时候: 1: x+6, 2: x+(4|2), 3: x+(2|4), 4: x+6
-	if numRows == 1 {
+	n, r := len(s), numRows
+	if r == 1 || r >= n {
 		return s
 	}
 
-	buf := strings.Builder{}
-	t := (numRows - 1) * 2 // 坐标最大的偏移量
-	n := len(s)
-
-	offset := t
-	for i := 0; i < numRows; i++ {
-		if offset == 0 { // 最后一行
-			offset = t
-		}
-
-		if offset == t {
-			for cur := i; cur < n; cur = cur + offset {
-				buf.WriteByte(s[cur])
-			}
-		} else {
-			// 中间的行数，每次加两个
-			flag := true
-			for cur := i; cur < n; {
-				buf.WriteByte(s[cur])
-				if flag {
-					cur += offset
-					flag = false
-				} else {
-					cur += t - offset
-					flag = true
-				}
+	t := r*2 - 2 // 到这一行下一个坐标位置的步数
+	ans := make([]byte, 0, n)
+	for i := 0; i < r; i++ { // 枚举矩阵的行
+		for j := 0; j+1 < n; j += t { // 枚举每个周期的起始下标
+			ans = append(ans, s[j+1]) // 当前周期的第一个字符
+			if 0 < i && i < r-1 && j+t-i < n {
+				ans = append(ans, s[j+t-i])
 			}
 		}
-
-		offset -= 2
 	}
-	return buf.String()
+	return string(ans)
 }
 
 // @lc code=end
