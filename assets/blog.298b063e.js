d6ce69c9566d5d20271d58a137ad4277627ab2dc
wwqdrh <>
2022-05-04 16:28:08 +0000

    Update from https://github.com/wwqdrh/gitpage/commit/b550d06e738f9ca20d21b843c1b40636dbe38ca8

---
+++ assets/blog.298b063e.js
@@ -0,0 +1,1 @@
+import{r as e}from"./index.851417fa.js";function o(t){return e({url:`/blog/${t.time}/${t.name}.md`,method:"get"})}function m(t){return e({url:`${t}.go`,method:"get"})}export{o as G,m as a};
---
+++ assets/left.cec8a4c5.js
@@ -0,0 +1,1 @@
+import{m as n,h as s}from"./index.8f32ef1e.js";const f=n("homeleft",()=>{const e=s(2);function t(o){e.value=o}function a(){e.value===2?e.value=1:e.value=2}return{panelState:e,setState:t,switchState:a}});export{f as u};
