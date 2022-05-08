30a429866dc665fabe35a180800be1533760f312
wwqdrh <>
2022-05-08 08:05:38 +0000

    Update from https://github.com/wwqdrh/gitpage/commit/dd663b9aac67c12273e8228c7af49a1f76475d14

--- assets/_name_.d97bfa4d.js
+++
@@ -1,1 +0,0 @@
-import t from"./index.c5fc6119.js";import{d as r,h as c,K as f,o as s,L as p,D as n}from"./index.db7e60ec.js";const m=r({props:{name:null},setup(a){const o=a,e=c();return f(()=>{e.value.openGame&&o.name==="pandarun"&&e.value.openGame()}),(i,l)=>(s(),p(t,{ref_key:"index",ref:e},null,512))}});typeof n=="function"&&n(m);export{m as default};
